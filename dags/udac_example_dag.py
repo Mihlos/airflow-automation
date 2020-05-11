517263a4c572d402acad2854307a4a40b032267e
root <miguel.mish@gmail.com>
2020-05-11 20:54:07 +0000

    WIP StageTo RedshiftOperator

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -13,9 +13,10 @@ default_args = {
     'owner': 'Mihlos',
     'start_date': datetime(2019, 1, 12),
     'depends_on_past': False,
-#     'retires': 1,
-    'retry_delay': ,
-    'catchup': False 
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_failure':False
 }
 
 dag = DAG('udac_example_dag',
@@ -24,16 +25,51 @@ dag = DAG('udac_example_dag',
           schedule_interval='0 * * * *'
         )
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+start_operator = DummyOperator(task_id='Begin_execution',  
+                               dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    conn_id='aws_credentials'
+    redshift_conn_id='redshift',
+    table='staging_events',
+    sql= """
+    CREATE TABLE public.staging_events (
+	num_songs int4,
+	artist_id varchar(256),
+	artist_name varchar(256),
+	artist_latitude numeric(18,0),
+	artist_longitude numeric(18,0),
+	artist_location varchar(256),
+	song_id varchar(256),
+	title varchar(256),
+	duration numeric(18,0),
+	"year" int4
+    );
+"""
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    conn_id='aws_credentials'
+    redshift_conn_id='redshift',
+    table='staging_songs',
+    sql_create= """
+    CREATE TABLE public.staging_songs (
+	num_songs int4,
+	artist_id varchar(256),
+	artist_name varchar(256),
+	artist_latitude numeric(18,0),
+	artist_longitude numeric(18,0),
+	artist_location varchar(256),
+	song_id varchar(256),
+	title varchar(256),
+	duration numeric(18,0),
+	"year" int4
+    );
+    """
 )
 
 load_songplays_table = LoadFactOperator(
@@ -68,13 +104,15 @@ run_quality_checks = DataQualityOperator(
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
+# Execution flow
+# Load STG tables
 start_operator >> stage_events_to_redshift >> load_songplays_table
 start_operator >> stage_songs_to_redshift >> load_songplays_table
-
+# Load dimensionals
 load_songplays_table >> load_song_dimension_table >> run_quality_checks
 load_songplays_table >> load_user_dimension_table >> run_quality_checks
 load_songplays_table >> load_artist_dimension_table >> run_quality_checks
 load_songplays_table >> load_time_dimension_table >> run_quality_checks
-
+# Check data quality
 run_quality_checks >> end_operator
 
--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,23 +1,47 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        DELIMITER '{}'
+    """
 
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id='',
+                 conn_id ='',
+                 table='',
+                 sql='',
+                 s3_bucket="",
+                 s3_key="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.conn_id = conn_id
+        self.table = table
+        self.sql = sql
 
     def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redsifht_conn_id)
+        redshift.run(f'DROP TABLE IF EXISTS {self.table};')
+        sql = self.sql
+        redshift.run(sql)
+        
+        aws_hook = AwsHook(self.conn_id)
+        credentials = aws_hook.get_credentials()
+        
         self.log.info('StageToRedshiftOperator not implemented yet')
 
 
