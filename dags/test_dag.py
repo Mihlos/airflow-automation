3794bf3d7cbe159a7006e7f1e98b7a9118c4acb9
root <miguel.mish@gmail.com>
2020-05-16 15:15:49 +0000

    data quality

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -107,7 +107,15 @@ load_time_dimension_table = LoadDimensionOperator(
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    tables=['staging_songs',
+            'staging_events',
+            'songplays', 
+            'users',
+            'songs',
+            'artists',
+            'time']
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
